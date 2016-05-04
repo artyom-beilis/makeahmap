6ec3f5b4e81a54d77669342ab4a5b05f285a2845
Artyom Beilis <artyomtnk@yahoo.com>
2016-05-05 00:00:30 +0300

    Added CMake support

--- sources/build.bat
+++ sources/build.bat
@@ -1,5 +1,5 @@
-set CFLAGS=-std=c++0x -O3 -g -m32 -DDLL_EXPORT -DCURL_STATICLIB -Wall -Wextra -I E:\AH1-Terrain\code\packages\include
-set LINK=-L E:\AH1-Terrain\code\packages\lib -lcurl -lwinmm -lwldap32 -lws2_32 -ltiff -lz
+set CFLAGS=-std=c++0x -O3 -g -m32 -DDLL_EXPORT -DCURL_STATICLIB -Wall -Wextra -I C:\Users\artik\mingw\packages\include
+set LINK=-L C:\Users\artik\mingw\packages\lib -lcurl -lwinmm -lwldap32 -lws2_32 -ltiff -lz
 
 windres -F pe-i386 makeahmap.rc -O coff -o makeahmap.res
 g++ -c %CFLAGS% makeahmap.cpp -o makeahmap.o
